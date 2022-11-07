1896f2a22861d801bee34f5edb7fc3952b379c6b
MurodullayevZafarbek <murodullayevzafarbek@gmail.com>
2022-11-07 10:51:39 +0500

    only front

--- server/routes/Error.js
+++
@@ -1,9 +0,0 @@
-const { Router } = require('express')
-const router = Router()
-
-/*Error page*/
-router.get('/',(req,res,next)=>{
-   res.json({message:"Page not found"}).status(404)
-})
-
-module.exports = router
